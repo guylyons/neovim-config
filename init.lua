fd0003580cf8bea3079bc9c526cc6fa79c134380
Guy Lyons <guylyons2@gmail.com>
2024-12-27 08:35:12 -0500

    comment

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- Guy Lyons
+-- Custom NeoVim configuration
+-- 2025
+--
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
