e1c0af7e34fdc95235a1fa184b483b2847c29b6b
guylyons <glyons@cwsc.com>
2025-01-17 14:28:43 -0500

    blade

--- init.lua
+++ init.lua
@@ -77,7 +77,18 @@ require("lazy").setup({
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
-      "nvim-treesitter/nvim-treesitter"
+      "nvim-treesitter/nvim-treesitter",
+      config = function()
+        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+        parser_config.blade = {
+          install_info = {
+            url = "https://github.com/EmranMR/tree-sitter-blade",
+            files = { "src/parser.c" },
+            branch = "main",
+          },
+          filetype = "blade, php",
+        }
+      end
     },
     {
       "nvim-neo-tree/neo-tree.nvim",
