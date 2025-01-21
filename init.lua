7bb8d216fc98896c8fcad0e6a80a0198217339aa
guylyons <glyons@cwsc.com>
2025-01-21 13:37:14 -0500

    Add Neogit

--- init.lua
+++ init.lua
@@ -136,6 +136,19 @@ require("lazy").setup({
       'hrsh7th/cmp-vsnip',
       'hrsh7th/vim-vsnip',
     },
+    {
+      "NeogitOrg/neogit",
+      dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",              -- optional
+        "echasnovski/mini.pick",         -- optional
+      },
+      config = true
+    }
   },
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
