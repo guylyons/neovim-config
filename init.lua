cdda7d3b11975e4e47fc3e2c950bcc25020fc980
guylyons <glyons@cwsc.com>
2025-01-21 13:58:56 -0500

    Shorten commands

--- init.lua
+++ init.lua
@@ -158,10 +158,10 @@ require("lazy").setup({
 
 -- Telescope default keymap
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- Folds with Tree-sitter
 vim.opt.foldmethod = "expr"
