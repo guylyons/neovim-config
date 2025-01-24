af1ba22998fc028005f67aa22a99f2bc8f7a260b
guylyons <glyons@cwsc.com>
2025-01-24 12:49:54 -0500

    fzf keymap

--- init.lua
+++ init.lua
@@ -156,9 +156,12 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
+local fzf = require('fzf-lua')
+vim.keymap.set('n', '<leader>f', fzf.files, { desc = 'Fzf files' })
+
 -- Telescope default keymap
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
