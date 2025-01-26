68c476ea7afea926b19c945179f7bf60c4771f69
Guy Lyons <guylyons2@gmail.com>
2025-01-26 18:02:02 -0500

    Neotree kb

--- init.lua
+++ init.lua
@@ -161,6 +161,7 @@ local fzf = require('fzf-lua')
 vim.keymap.set('n', '<leader>f', fzf.files, { desc = 'Fzf files' })
 vim.keymap.set('n', '<leader>k', fzf.lines, { desc = 'Fzf lines' })
 vim.keymap.set('n', '<leader>m', ':Neogit<CR>', { desc = 'Fzf lines' })
+vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>', { desc = 'Opens Neotree' })
 
 -- Telescope default keymap
 local builtin = require('telescope.builtin')
