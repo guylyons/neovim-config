d766211beb43e8e361133d51c88bd02b007f5d98
Guy Lyons <guylyons2@gmail.com>
2025-01-27 21:58:40 -0500

    vim register keybinding

--- init.lua
+++ init.lua
@@ -175,6 +175,7 @@ vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = 'Telescope find fi
 vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>v', builtin.registers, { desc = 'Telescope help tags' })
 
 -- Folds with Tree-sitter
 vim.opt.foldmethod = "expr"
