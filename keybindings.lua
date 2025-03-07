8c61ee0988c85d4662b23324c50c07643bd18b09
Guy Lyons <guylyons2@gmail.com>
2025-03-07 12:31:19 -0500

    ianother one

--- keybindings.lua
+++ keybindings.lua
@@ -13,18 +13,6 @@ vim.keymap.set('n', '<leader>j', ':Yazi<CR>', { desc = 'Opens Yazi' })
 -- Ex
 vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Opens Ex' })
 
--- Automatically return to the last editing position when reopening a file
-vim.api.nvim_create_autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local line = mark[1]
-    local col = mark[2]
-    if line > 0 and line <= vim.api.nvim_buf_line_count(0) then
-      vim.api.nvim_win_set_cursor(0, { line, col })
-    end
-  end,
-})
-
 -- FZF lua
 local fzf = require('fzf-lua')
 vim.keymap.set('n', '<leader>f', fzf.files, { desc = 'Fzf files' })
--- settings.lua
+++ settings.lua
@@ -33,3 +33,15 @@ vim.api.nvim_create_autocmd('CursorMoved', {
   end
 })
 
+-- Automatically return to the last editing position when reopening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line = mark[1]
+    local col = mark[2]
+    if line > 0 and line <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, { line, col })
+    end
+  end,
+})
+
