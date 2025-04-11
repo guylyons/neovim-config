36b5435df807ac6a4c8d33d32e2b2b4efa46715f
guylyons <glyons@cwsc.com>
2025-04-11 16:36:51 -0400

    fix

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,8 @@
 return {
   'stevearc/conform.nvim',
   opts = {
+  },
+  config = function ()
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
@@ -17,5 +19,5 @@ return {
         },
       },
     })
-  },
+  end
 }
