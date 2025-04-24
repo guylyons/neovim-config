ec444d224e8f751932654024a2a820993d14ea56
Guy Lyons <guylyons2@gmail.com>
2025-04-24 09:02:21 -0400

    inline errors

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,24 +1,31 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        },
-      },
-    },
-  },
-  config = function()
-    -- Lua language server setup
-    require'lspconfig'.lua_ls.setup{}
-    -- Python
-    require'lspconfig'.pyright.setup{}
-    -- Bash
-    require'lspconfig'.bashls.setup{}
-  end,
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+	},
+	config = function()
+		-- Lua language server setup
+		require("lspconfig").lua_ls.setup({})
+		-- Python
+		require("lspconfig").pyright.setup({})
+		-- Bash
+		require("lspconfig").bashls.setup({})
+
+		vim.diagnostic.config({
+			virtual_text = true,
+			signs = false,
+			underline = false,
+			update_in_insert = false,
+		})
+	end,
 }
