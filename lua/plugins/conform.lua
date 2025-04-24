a47f0cc7bd3397960949b7e454398e0feae94ed4
Guy Lyons <guylyons2@gmail.com>
2025-04-24 08:57:22 -0400

    conform, format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -19,5 +19,11 @@ return {
         },
       },
     })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+      end,
+    })
   end
 }
