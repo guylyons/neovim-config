cbc4d14c57a848cc6f02b8621240c0239fb4a377
guylyons <glyons@cwsc.com>
2025-08-07 09:59:20 -0400

    add twig to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "tsx", "typescript", "javascript", "html", "css" },
+			ensure_installed = { "tsx", "typescript", "javascript", "twig", "html", "css" },
 			highlight = { enable = true },
 			indent = { enable = true }, -- This is the key line
 		})
