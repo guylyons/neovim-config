fbfa1f135137958d6f221d6d59c87ba00314e131
guylyons <glyons@cwsc.com>
2025-08-07 10:05:33 -0400

    add blade

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "tsx", "typescript", "javascript", "twig", "html", "css" },
+			ensure_installed = { "tsx", "typescript", "javascript", "twig", "blade", "html", "css" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
