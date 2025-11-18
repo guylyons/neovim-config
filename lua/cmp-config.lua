5b86d17d77112c824c600be54487e9628b8f0315
Guy Lyons <guylyons2@gmail.com>
2025-11-18 12:07:23 -0500

    treesitter

--- lua/cmp-config.lua
+++ lua/cmp-config.lua
@@ -1,33 +1,33 @@
 -- Completion setup for `nvim-cmp`
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For `vsnip` users
-  }, {
-    { name = 'buffer' },
-  }),
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" }, -- For `vsnip` users
+	}, {
+		{ name = "buffer" },
+	}),
 })
 
 -- For `/` and `?` in cmdline
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  },
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 -- For `:` in cmdline
@@ -39,4 +39,3 @@ cmp.setup.cmdline({ '/', '?' }, {
 --     { name = 'cmdline' }
 --   }),
 -- })
-
--- lua/plugins/material.lua
+++ lua/plugins/material.lua
@@ -1,20 +1,20 @@
 return {
-  "marko-cerovac/material.nvim",
-  lazy = false, -- Load it immediately
-  priority = 1000, -- Load this before other plugins to apply the theme
-  config = function()
-    require('material').setup({
-      contrast = {
-        terminal = false, -- Enable contrast for terminal
-        sidebars = true, -- Enable contrast for sidebars like NvimTree
-        floating_windows = true, -- Enable contrast for floating windows
-      },
-      styles = {
-        comments = { italic = true },
-        keywords = { bold = true },
-        functions = { bold = true, italic = true },
-      },
-    })
-    vim.cmd("colorscheme material-deep-ocean") -- Set the theme
-  end,
+	"marko-cerovac/material.nvim",
+	lazy = false, -- Load it immediately
+	priority = 1000, -- Load this before other plugins to apply the theme
+	config = function()
+		require("material").setup({
+			contrast = {
+				terminal = false, -- Enable contrast for terminal
+				sidebars = true, -- Enable contrast for sidebars like NvimTree
+				floating_windows = true, -- Enable contrast for floating windows
+			},
+			styles = {
+				comments = { italic = true },
+				keywords = { bold = true },
+				functions = { bold = true, italic = true },
+			},
+		})
+		vim.cmd("colorscheme material-deep-ocean") -- Set the theme
+	end,
 }
