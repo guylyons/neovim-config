08af84ca006101fe501863174d5b826f8443ee10
guylyons <glyons@cwsc.com>
2025-11-25 11:03:56 -0500

    remove comment

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
       ensure_installed = {
         "lua",
         "javascript",
-        "typescript", -- optional but useful
+        "typescript",
       },
       highlight = {
         enable = true,
