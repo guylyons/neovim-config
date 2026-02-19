1c95fc84ad4573cdf6eeb3bac56fa2c311ad6efa
Guy Lyons <guylyons2@gmail.com>
2026-02-19 00:44:03 -0500

    fix

--- lua/plugins/drupal.lua
+++ lua/plugins/drupal.lua
@@ -2,16 +2,4 @@ return {
 	"jdrupal-dev/drupal.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	ft = { "php", "twig" },
-	config = function()
-		local ok, drupal = pcall(require, "drupal")
-		if not ok then
-			return
-		end
-		drupal.setup({
-			services_cmp_trigger_character = "@",
-			get_drush_executable = function(_current_dir)
-				return "drush"
-			end,
-		})
-	end,
 }
