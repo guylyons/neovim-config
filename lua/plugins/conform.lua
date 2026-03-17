5104588ebad186988325d3c3227f500e91e3fcda
Guy Lyons <guylyons2@gmail.com>
2026-03-17 08:45:49 -0400

    indent

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,13 +1,13 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				rust = { "rustfmt" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        rust = { "rustfmt" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    })
+  end,
 }
